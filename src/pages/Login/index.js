b32af3d721bf82b5537e973117ab68cd51202244
ivanpazl2code <ivan.paz@l2code.com.br>
2021-02-08 12:27:52 -0300

    Novo css

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -2,10 +2,11 @@ import React, {useState} from 'react';
 import {useHistory} from 'react-router-dom';
 import api from '../../services/api';
 import './styles.css';
+import '../../global.css';
 
-import Api from '../../services/api.js';
 
-import { Button } from 'react-bootstrap';
+
+
 import backImage from '../../assets/login_comp.png';
 import boxImage from '../../assets/login_box.png';
 
@@ -36,11 +37,11 @@ function Login(){
             console.log(name, password);
             const response = await api.post('login', {name, password});
            console.log(response.data);
-           /* localStorage.setItem('userId', response.data[0].id);
-            localStorage.setItem('userName', response.data[0].name);
-            localStorage.setItem('userCapitan', response.data[0].capitan);
-            localStorage.setItem('userImage', response.data[0].image);
-            localStorage.setItem('userScore', response.data[0].score);*/
+           localStorage.setItem('userId', response.data.id);
+            localStorage.setItem('userName', response.data.name);
+            localStorage.setItem('userCapitan', response.data.capitan);
+            localStorage.setItem('userImage', response.data.image);
+            localStorage.setItem('userScore', response.data.score);
 
             history.push('/menu');
             
@@ -60,15 +61,20 @@ function Login(){
 
                 <div className="formulario">
                     <form action="" className="form-container" onSubmit={handleLogin}>
-                        <div className="campo">
-                            <div className="linha-texto">Batalhão</div>
-                            <input type="text" placeholder="Nome do Batalhão"  value={name} onChange={e => setName(e.target.value)}  />                            
-                        </div>
+                        
 
-                        <div className="campo">
-                            <div className="linha-texto">Senha</div>
-                            <input type="text" placeholder="Password" value={password} onChange={e => setPassword(e.target.value)} />                            
-                        </div>
+                        
+                            <div className="campo campo-login">
+                                <div className="linha-texto">Batalhão</div>
+                                <input type="text" placeholder="Nome do Batalhão"  value={name} onChange={e => setName(e.target.value)}  />                            
+                            </div>
+
+                            <div className="campo  campo-login">
+                                <div className="linha-texto">Senha</div>
+                                <input type="text" placeholder="Password" value={password} onChange={e => setPassword(e.target.value)} />                            
+                            </div>
+                        
+                        
 
                         <button type="submit" class="btn-circle btn-login">Entrar</button>
                         {isWrong && <div className="error-login">Usuário e/ou senha incorreto.</div>     }
--- src/pages/Menu/index.js
+++ src/pages/Menu/index.js
@@ -1,12 +1,21 @@
-import React, {useEffect} from 'react';
+import React, {useEffect,useState} from 'react';
 import './styles.css';
 import {Link} from 'react-router-dom';
 import boxImage from '../../assets/menu_box.png';
 import MenuBar from './menu-bar';
 
 function Menu() {
+    const [teamName, setTeamName] = useState('');
+    const [teamCapitan, setTeamCapitan] = useState('');
+    const [teamScore, setTemaScore] = useState();
 
-    
+    useEffect(() => {
+
+        setTeamName(localStorage.getItem('userName'));
+        setTeamCapitan(localStorage.getItem('userCapitan'));
+        setTemaScore(localStorage.getItem('userScore'));
+       
+      });
    
     return(
 
@@ -15,13 +24,21 @@ function Menu() {
                 <MenuBar />
                 <div className="body">
                     <div className="user-info">
-                        <div className="user-info-left">
-                            <img src="" alt="" className="brasao"/>
+                        <div className="user-info-item user-info-left">
+                            <img src="https://drive.google.com/uc?id=1y3VgFeEAnSqvFqhLye5w6lOFxuD330wC" alt="" className="brasao"/>
                         </div>
-                        <div className="user-info-right">
-                            <div className="info-team"></div>
-                            <div className="info-capitan"></div>
-                            <div className="info-score"></div>
+                        <div className="user-info-item user-info-right">
+                            <div className="campo campo-menu">
+                                <div className="linha-texto-menu">Batalhão: <b>{teamName}</b></div>                                                            
+                            </div>
+                            <div className="campo campo-menu">
+                                <div className="linha-texto-menu">Capitão: <b>{teamCapitan}</b></div>                                                            
+                            </div>
+                            <div className="campo campo-menu">
+                                <div className="linha-texto-menu">Score: <b>{teamScore} pts</b></div>                                                            
+                            </div>
+                            
+                            
                         </div>
                     </div>
                 </div>
