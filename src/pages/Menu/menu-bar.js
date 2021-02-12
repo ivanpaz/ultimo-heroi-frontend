2084a84bbb9d1e73289471b8a0e487b5fad47573
ivanpazl2code <ivan.paz@l2code.com.br>
2021-02-12 18:19:24 -0300

    updfat

--- src/pages/Menu/menu-bar.js
+++ src/pages/Menu/menu-bar.js
@@ -5,18 +5,18 @@ import {Link} from 'react-router-dom';
 function MenuBar(){
     return(
         <div className="btn-bar">
-                    <Link className="btn-mounth" to='/Mission/1' /> 
-                    <Link className="btn-mounth" to='/Mission/2' /> 
-                    <Link className="btn-mounth" to='/Mission/3' /> 
-                    <Link className="btn-mounth" to='/Mission/4' />  
-                    <Link className="btn-mounth" to='/Mission/5' /> 
-                    <Link className="btn-mounth" to='/Mission/6' /> 
-                    <Link className="btn-mounth" to='/Mission/7' /> 
-                    <Link className="btn-mounth" to='/Mission/8' /> 
-                    <Link className="btn-mounth" to='/Mission/9' /> 
-                    <Link className="btn-mounth" to='/Mission/10' /> 
-                    <Link className="btn-mounth" to='/Mission/11' /> 
-                    <Link className="btn-mounth" to='/Mission/12' /> 
+                    <Link className="btn-mounth m1" to='/Mission/1' /> 
+                    <Link className="btn-mounth m2" to='/Mission/2' /> 
+                    <Link className="btn-mounth m3" to='/Mission/3' /> 
+                    <Link className="btn-mounth m4" to='/Mission/4' />  
+                    <Link className="btn-mounth m5" to='/Mission/5' /> 
+                    <Link className="btn-mounth m6" to='/Mission/6' /> 
+                    <Link className="btn-mounth m7" to='/Mission/7' /> 
+                    <Link className="btn-mounth m8" to='/Mission/8' /> 
+                    <Link className="btn-mounth m9" to='/Mission/9' /> 
+                    <Link className="btn-mounth m10" to='/Mission/10' /> 
+                    <Link className="btn-mounth m11" to='/Mission/11' /> 
+                    <Link className="btn-mounth m12" to='/Mission/12' /> 
         </div>
     );
 
--- src/pages/Mission/element.js
+++ src/pages/Mission/element.js
@@ -1,28 +1,66 @@
 import React, {useState, useEffect} from 'react';
+import api from '../../services/api';
 import './styles.css';
 
 function Element(props){
+    const [mission, setMission] = useState({});
+
+    const [newDone, setDone] = useState(false);
+    const [newFfeedback, setFeedback] = useState('');
 
     useEffect(() => {
-       
         
-
-        console.log(props.mission);
-        
-        
+       setMission(props.mission);
+       setDone(props.mission.done);
+       setFeedback(props.mission.feedback);
+       console.log(props.mission);
         
       }, []);
+
+    const onItemChecked = (e)=>{
+        setDone(e.target.checked);
+    };
+
+    const onTextChange =(e) => {
+        setFeedback(e.target.value);        
+    };
+
+    
+
+    async function UpdateMIssion(){
+        
+        const url = `missions/update`;
+        console.log(props.mission._id);
+        console.log(newDone);
+        console.log(newFfeedback);
+        const response = await api.get(`missions/update`,  { headers:{},
+            data: {
+                done: newDone,
+	            feedback: newFfeedback,
+	            anex: 'sem link', 
+	            _id: props.mission._id
+            },
+          });
+
+        console.log(response.data);
+       
+        
+      }
 
     return(
 
         <div class="mission-element">
             <div className="mission-number">
-                <div className="mission-title">Missão {props.mission.numMission}</div>
-                <input type="checkbox" name="" id=""/>
+                <div className="mission-title">Missão {mission.numMission}</div>
+                <input type="checkbox" checked={newDone} onChange={onItemChecked}/>
             </div>
             <div className="mission-area">
-                <textarea name="" id="" cols="30" rows="5">{props.mission.feedback}</textarea>
-                <div className="anex-btn">anexar</div>
+                <textarea name="" id="" cols="30" rows="5" value={newFfeedback} onChange={onTextChange}/>
+                <div className="mission-btns">
+                    <div className="anex-btn">anexar</div>
+                    <div className="save-btn" onClick={UpdateMIssion}></div>
+                </div>
+                
             </div>
         </div>
     );
